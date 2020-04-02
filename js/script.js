e8dcc0a21a17cd5c71b903106a8c0c7a160c3128
banyurachman <banyurachman95@gmail.com>
2020-04-03 03:12:11 +0700

    Small addition for fun

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    console.log("What are you looking for here? -BR");
+
     $('#mainSegment img').on('click', function () {
         window.open("https://www.instagram.com/p/B7vcShEHR0Z/", "_blank");
     });
